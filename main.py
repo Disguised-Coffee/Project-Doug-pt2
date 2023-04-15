7e77fb287586e94a7c7f61e555c32238fb482ddc
Disguised Coffee <68920714+Disguised-Coffee@users.noreply.github.com>
2023-04-15 20:35:19 +0000

    Started database integration

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+"""
+ Sample 
+  {
+    "link":"https://www.youtube.com/embed/QR3xhMsk2Ak",
+    "id":0,
+    "caption":"Hello World!",
+    "name":"DOG"
+  }
+"""
+
+BS_CODE = [
+  {
+    "link":"https://www.youtube.com/embed/QR3xhMsk2Ak",
+    "id":0,
+    "caption":"Hello World!",
+    "name":"DOG"
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html',jobs=BS_CODE)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(BS_CODE)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
